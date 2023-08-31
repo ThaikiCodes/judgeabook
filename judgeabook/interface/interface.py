c6fe41fe2b55586a10630512844ec2fe473c9687
yarenmerveakin <akinya@mef.edu.tr>
2023-08-31 17:48:36 +0200

    added api part

--- judgeabook/interface/interface.py
+++ judgeabook/interface/interface.py
@@ -2,52 +2,68 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import pandas as pd
+import requests
+
+url = 'http://localhost:8502/'  #ERROR HERE
+
 
 st.markdown("""# Judging a book by its cover.
 ''Who are you?'''
 
 By analyzing your facial features, your personal characteristic traits will be predicted based on Chinese Horoscopes.""")
 
-
-
-
 image = Image.open('/Users/yaren/Desktop/Zodiac-Wheel-2021-2022.jpg')
 
 st.image(image, caption='Chinese Zodiac Signs based on years')
 
+if st.button('Upload'):
+    st.write('Your character traits are being predicted!')
+else:
+     print('')
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-uploaded_file = st.file_uploader("Please upload your picture", type="jpg")
+uploaded_file = st.file_uploader("Please upload your picture", type=['jpeg','jpg','png'])
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data)
+    data_bytes = uploaded_file.getvalue()
+    st.image(data_bytes)
+    st.write(data_bytes)
 
-if st.button('Upload'):
-    print('button clicked!')
-    st.write('Please wait 🎉')
-    st.write('Your character traits are being predicted!')
-else:
-    st.write('Please try again 😞')
+    files = {"file" : (data_bytes)}
+    response = requests.post(url, files=files)
+
+    if response.status_code ==200 :
+        result = response.json()
+        st.succes('Image uploaded!')
+        st.write('API response : ', result)
+    else:
+        st.error('Failed to upload image.')
 
 
 
-def add_bg_from_url():   ######NOT WORKING
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image:("/Users/yaren/Desktop/Chinese-zodiac-signs-Graphics-7010501-1.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
 
-    return add_bg_from_url()
+
+
+
+
+
+# def add_bg_from_url():   ######NOT WORKING
+#     st.markdown(
+#          f"""
+#          <style>
+#          .stApp {{
+#              background-image:("/Users/yaren/Desktop/Chinese-zodiac-signs-Graphics-7010501-1.jpg");
+#              background-attachment: fixed;
+#              background-size: cover
+#          }}
+#          </style>
+#          """,
+#          unsafe_allow_html=True
+#      )
+
+#     return add_bg_from_url()
 
 
 
@@ -57,16 +73,7 @@ def add_bg_from_url():   ######NOT WORKING
 #data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
 #st.write(data)
 
-
-#E9967A
-#E9E9E80
-
-
-
-
-
-
 # Loading Image using PIL
-im = Image.open('/Users/yaren/Desktop/Chinese-zodiac-signs-Graphics-7010501-1.jpg')
+# im = Image.open('/Users/yaren/Desktop/Chinese-zodiac-signs-Graphics-7010501-1.jpg')
 
-st.set_page_config(page_title="Surge Price Prediction App", page_icon = im)
+# st.set_page_config(page_title="Surge Price Prediction App", page_icon = im)
